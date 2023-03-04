2a0e975039a09c23b0136204d00208d762c00bea
Danielle Van Boxel <the.dvb@gmail.com>
2023-03-04 10:01:54 -0700

    Add source code files to docs

--- docs/pub_docs.sh
+++ docs/pub_docs.sh
@@ -3,4 +3,4 @@
 rm -rf build
 make html
 cp -r build/html/* ./
-git add *.html searchindex.js objects.inv _sources/*.txt source/*.rst
+git add *.html searchindex.js objects.inv _sources/*.txt source/*.rst _modules/index.html _modules/barmpy/*.html
