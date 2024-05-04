dba0ccb31859eaefa95447ebff4da3e5bb062651
Danielle Van Boxel <the.dvb@gmail.com>
2024-05-04 13:47:35 -0700

    Update version and email

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='barmpy',
-    version='1.0.0',
+    version='1.1.0',
     description='Bayesian Additive Regression Models implementation for Python',
     url='https://github.com/dvbuntu/barmpy',
     author='Danielle Van Boxel',
-    author_email='vanboxel@math.arizona.edu',
+    author_email='vanboxel@arizona.edu',
     packages=find_packages(),
     install_requires=[
         'matplotlib',
